751ca0a87ee49b60440579d4d422e13b7d2889a0
ethanlee <seungwoonlee90@gmail.com>
2021-09-16 17:37:55 +0900

    create router detail pages

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js'
-import Card from './components/Card.js'
+import Card from './components/Card.js';
+
+import { Route } from 'react-router-dom';
 
 function App() {
 
@@ -30,23 +32,38 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      <div className="background">
-        <h3>20% Season Off</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        <Button variant="primary">Learn more</Button>
-      </div>
-
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((shoe, i) => {
-              return <Card shoe={shoes[i]} id={i} key={i}/>
-            })
-          }
+      <Route exact path="/">
+        <div className="background">
+          <h3>20% Season Off</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Button variant="primary">Learn more</Button>
         </div>
-      </div>
 
+        <div className="container">
+          <div className="row">
+            {
+              shoes.map((shoe, i) => {
+                return <Card shoe={shoes[i]} id={i} key={i}/>
+              })
+            }
+          </div>
+        </div>
+      </Route>
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="detail_shoe_1" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div> 
+      </Route>
     </div>
   );
 }
