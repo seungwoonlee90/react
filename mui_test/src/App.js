34fdf98f9e3474f9b5f53d68e7447bf54ad2f4f7
ethanlee <seungwoonlee90@gmail.com>
2022-02-02 00:21:35 +0900

    Update Container

--- mui_test/src/App.js
+++ mui_test/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ethan's react MUI</h1>
-      <TextField id="standard-basic" label="id" variant="standard" />
-      <TextField id="standard-basic" label="password" variant="standard" />
-      <Button variant="outlined">Log In</Button>
+      <Container fixed>
+        <h1>Ethan's react MUI</h1>
+        <TextField id="standard-basic" label="id" variant="standard" />
+        <TextField id="standard-basic" label="password" variant="standard" />
+        <Button variant="outlined">Log In</Button>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </Container>
     </div>
   );
 }
