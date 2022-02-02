c282ec976cb363131fd722be6d36dfe20ec77977
ethanlee <seungwoonlee90@gmail.com>
2022-02-02 21:20:40 +0900

    MUI Sign In

--- mui_test/src/App.js
+++ mui_test/src/App.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
 
 function App() {
   return (
     <div className="App">
-      <Container fixed>
+      <Container maxWidth="xs">
         <h1>Ethan's react MUI</h1>
-        <TextField id="standard-basic" label="id" variant="standard" />
-        <TextField id="standard-basic" label="password" variant="standard" />
-        <Button variant="outlined">Log In</Button>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <TextField id="email"
+        margin="normal"
+        label="Email Address"
+        autoComplete="email"
+        required
+        fullWidth
+        autoFocus
+        name="email"
+        variant="standard" />
+        <TextField id="password"
+        margin="normal"
+        label="Password"
+        type="password"
+        fullWidth
+        name="password"
+        variant="standard" />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} >Log In</Button>
+        <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+          </Grid>
       </Container>
     </div>
   );
